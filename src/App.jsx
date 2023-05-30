e8c274d716e58faeed6464a5f980178981309434
TamaraFerr <tamarablacksmit@hotmail.com>
2023-05-29 23:43:45 -0300

    putting the libs and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-
-
+import Cadastro from "./pages/Cadastro/Cadastro"
+import Login from "./pages/Login/Login"
+import Habitos from "./pages/Habitos"
+import Hoje from "./pages/Hoje"
+import Historico from "./pages/Historico"
+import NavBar from "./components/NavBar"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+ 
 function App() {
   return (
+    <BrowserRouter>
+
+      <NavBar />
+
+      <Routes>
+        <Route path="/" element={<Login />}/>
+        <Route path="/cadastro" element={<Cadastro />}/>
+        <Route path="/hoje" element={<Hoje />}/>
+        <Route path="/habitos" element={<Habitos />}/>
+        <Route path="/historico" element={<Historico />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
